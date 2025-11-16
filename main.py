a36c119280fc787fb4a46b084dfda44e44cd2f82
ImanuelChatur <charliechatur@gmail.com>
2025-11-16 14:45:29 -0500

    Hu moments calculated

--- main.py
+++ main.py
@@ -1,14 +1,50 @@
-# This is a sample Python script.
+import cv2
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+##Encoding
+#1. Calculate Hu moments
+#2. Embed watermark
+#3. Create Hash
+#4. Embed hash + hu moments
+
+def calculate_hu_moments(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 150, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    print("Number of Contours detected:", len(contours))
+
+    cnt = contours[0]
+    M = cv2.moments(cnt)
+    Hm = cv2.HuMoments(M) #Hu moments
+
+    for i, cont in enumerate(contours):
+        cv2.drawContours(img, cont, -1, (0, 255, 255), 3)
+        x, y = cont[0,0]
+        #cv2.putText(img, f'Contour {i}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print("Hu-Moments of first contour:\n", Hm)
+    cv2.imshow("Hu-Moments", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return Hm
+
+#Embed Watermark
+def embed_watermark(img):
+    pass
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    print("Hello World")
+    img_name = 'images/kelpy.png'
+
+    img = cv2.imread(img_name)
+
+    #Get hu moments
+    hu_moments = calculate_hu_moments(img)
+
+    #Embed Watermark
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
